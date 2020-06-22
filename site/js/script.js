3573f0cd9a258e06ba3087365e70bb736f69ce31
Rohan George D <rohan.geo.2001@gmail.com>
2020-06-22 21:29:27 +0530

    Updated the js code to work appropriately

--- site/js/script.js
+++ site/js/script.js
@@ -1,25 +1,37 @@
-function addone(){
-            var no = document.getElementById('op').innerHTML;
-            no ++;
-            document.getElementById('op').innerHTML = no;
-        }
-        function subtractone(){
-            var no = document.getElementById('op').innerHTML;
-            no --;
-            document.getElementById('op').innerHTML = no;
-        }
-        function reset(){
-            var no = document.getElementById('op').innerHTML;
-            document.getElementById('op').innerHTML = 25;
-        }
-        var counter = 25;
-        function start(){setInterval(function(){
+function addone()
+    {
+        var no = document.getElementById('op').innerHTML;
+         no ++;
+         document.getElementById('op').innerHTML = no;
+    }
+function subtractone()
+    {
+        var no = document.getElementById('op').innerHTML;
+         no --;
+        document.getElementById('op').innerHTML = no;
+    }
 
+        
+ function start()
+    {
+        var counter = document.getElementById('op').innerHTML;
+        var myvar = setInterval(function(){
             counter --;
-            if(counter >=0){
+            if(counter >=0)
+            {
             var no = document.getElementById('op').innerHTML;
-            document.getElementById('op').innerHTML = counter;}
+            document.getElementById('op').innerHTML = counter;
+            }
             else
-                {alert('sorry, out of time');
-                counter = 25;}
-        },500);}
+                {
+                    alert('sorry, out of time');
+                    reset();
+                    clearInterval(myvar);
+                }
+        },500);
+    }
+    function reset()
+    {
+        document.getElementById('op').innerHTML = 25;
+    }
+    
